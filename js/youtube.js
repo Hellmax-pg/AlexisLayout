2b7ae7166816a36152b5e9174bf3968b84b193ba
hellmax <pegashev95@gmail.com>
2021-03-13 22:13:01 +0200

    add images

---
+++ js/youtube.js
@@ -0,0 +1,9 @@
+$('.click-video-button').click(function() {
+    $('.click-for-video').css('display', 'none');
+    $('div.youtube').css('display', 'block');
+    $('iframe.youtube').prop('src', 'https://www.youtube.com/embed/ZOzzRlc_qho?rel=0&amp;modestbranding=1&amp;controls=2&amp;showinfo=0&amp;fs=1&amp;wmode=opaque&amp;autoplay=1&amp;enablejsapi=1&amp;');
+});
+
+// $('.portfolio-img-block').hover(function() {
+//     $('.portfolio-img-block-text').html('Project One')
+// });

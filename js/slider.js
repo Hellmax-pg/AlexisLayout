252922b3d1d3c101f1a29be2ba1d7ae557186186
hellmax <pegashev95@gmail.com>
2021-03-15 00:49:35 +0200

    add last pages,img and footer

--- js/slider.js
+++ js/slider.js
@@ -26,13 +26,9 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // slideIndex++;
-    // if (slideIndex > slides.length) {slideIndex = 1}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "flex";
     dots[slideIndex - 1].className += " active";
 }
-
-// setInterval(showSlides, 6000);
